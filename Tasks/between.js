39d0f8c60a63ac840284d1edf3c0bdd6bf3642ff
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-04-28 17:46:58 +0300

    Added comment. Renamed misunderstanding variables.

--- Tasks/between.js
+++ Tasks/between.js
@@ -1,14 +1,15 @@
 'use strict';
 
+// Extract substring between prefix and suffix
+
 const getvaluebetween = (string, prefix, suffix) => {
   let prefixBeginIndex = 0;
   let isPrefixFound = false;
 
-  for (let i = 0; i < string.length; i += 1) {
-    if (string[i] === prefix) {
+  for (let i = 0; i < string.length / 2; i += 1) {
+    if (!isPrefixFound && string[i] === prefix) {
       prefixBeginIndex = i;
       isPrefixFound = true;
-      break;
     }
   }
 
@@ -16,17 +17,18 @@ const getvaluebetween = (string, prefix, suffix) => {
 
   const indexAfterPrefix = prefixBeginIndex + prefix.length;
   string = string.substring(indexAfterPrefix);
+
   if (suffix) {
-    const prefixBeginIndex = string.indexOf(suffix);
-    if (prefixBeginIndex === -1) {
+    const suffixBeginIndex = string.indexOf(suffix);
+    if (suffixBeginIndex === -1) {
       return '';
     } else {
-      string = string.substring(0, prefixBeginIndex);
+      string = string.substring(0, suffixBeginIndex);
     }
   }
 
+  console.log(string);
   return string;
 };
 
-getvaluebetween('Hello <username> and bye!', '<', '>');
 require('../Tests/between.js')(getvaluebetween);
