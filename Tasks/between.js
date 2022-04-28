7803f918c2e2ed22ae57473f0998a7124b6f9f00
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-04-28 17:57:27 +0300

    Changed search substring principle. Added some variables.

--- Tasks/between.js
+++ Tasks/between.js
@@ -3,32 +3,26 @@
 // Extract substring between prefix and suffix
 
 const getvaluebetween = (string, prefix, suffix) => {
-  let prefixBeginIndex = 0;
+  let prefixIndex = 0;
+  let suffixIndex = 0;
+
   let isPrefixFound = false;
+  let isSuffixFound = false;
 
   for (let i = 0; i < string.length / 2; i += 1) {
     if (!isPrefixFound && string[i] === prefix) {
-      prefixBeginIndex = i;
+      prefixIndex = i + 1;
       isPrefixFound = true;
+    }
+
+    if (!isSuffixFound && string[string.length - i - 1] === suffix) {
+      suffixIndex = string.length - i - 1;
+      isSuffixFound = true;
     }
   }
 
   if (!isPrefixFound) return '';
-
-  const indexAfterPrefix = prefixBeginIndex + prefix.length;
-  string = string.substring(indexAfterPrefix);
-
-  if (suffix) {
-    const suffixBeginIndex = string.indexOf(suffix);
-    if (suffixBeginIndex === -1) {
-      return '';
-    } else {
-      string = string.substring(0, suffixBeginIndex);
-    }
-  }
-
-  console.log(string);
-  return string;
+  return string.substring(prefixIndex, suffixIndex);
 };
 
 require('../Tests/between.js')(getvaluebetween);
