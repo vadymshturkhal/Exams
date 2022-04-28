c584bce08e0b26803f3208a4bec93ac7c2ae7d38
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-04-28 17:35:27 +0300

    Changed search for prefix. Removed else.

--- Tasks/between.js
+++ Tasks/between.js
@@ -1,21 +1,32 @@
 'use strict';
 
 const getvaluebetween = (string, prefix, suffix) => {
-  const prefixBeginIndex = string.indexOf(prefix);
-  if (prefixBeginIndex === -1) return '';
-  else {
-    const indexAfterPrefix = prefixBeginIndex + prefix.length;
-    string = string.substring(indexAfterPrefix);
-    if (suffix) {
-      const prefixBeginIndex = string.indexOf(suffix);
-      if (prefixBeginIndex === -1) {
-        return '';
-      } else {
-        string = string.substring(0, prefixBeginIndex);
-      }
+  let prefixBeginIndex = 0;
+  let isPrefixFound = false;
+
+  for (let i = 0; i < string.length; i += 1) {
+    if (string[i] === prefix) {
+      prefixBeginIndex = i;
+      isPrefixFound = true;
+      break;
     }
   }
+
+  if (!isPrefixFound) return '';
+
+  const indexAfterPrefix = prefixBeginIndex + prefix.length;
+  string = string.substring(indexAfterPrefix);
+  if (suffix) {
+    const prefixBeginIndex = string.indexOf(suffix);
+    if (prefixBeginIndex === -1) {
+      return '';
+    } else {
+      string = string.substring(0, prefixBeginIndex);
+    }
+  }
+
   return string;
 };
 
+getvaluebetween('Hello <username> and bye!', '<', '>');
 require('../Tests/between.js')(getvaluebetween);
