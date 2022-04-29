d52e549fef795c4d047ec98bc7a017ee5e128bbd
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-04-29 11:48:36 +0300

    Replased foreach with for...in.

--- Tasks/count.js
+++ Tasks/count.js
@@ -4,11 +4,12 @@
 
 const totalValuesSum = (sumValuesFrom) => {
   let sum = 0;
-  const keys = Object.keys(sumValuesFrom);
-  keys.forEach((key) => {
+
+  for (const key in sumValuesFrom) {
     const value = sumValuesFrom[key];
     if (typeof value === 'number') sum += value;
-  });
+  }
+
   return sum;
 };
 
