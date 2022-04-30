8b765af107948023f6e400ad2dfdb57c085c7847
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-04-30 10:57:46 +0300

    Changed signature. Changed variables names. Added lets/consts.

--- Tasks/difference.js
+++ Tasks/difference.js
@@ -1,19 +1,19 @@
+'use strict';
+
 // Find a difference between two dictionaries
 
-const diff = (DIFF = function diff(object_1, object_2) {
-  for (attribute_name in object_1) {
-    object_1[attribute_name] = object_1[attribute_name];
+const diff = (first, second) => {
+  for (const key in first) {
+    first[key] = first[key];
   }
-  {
+
+  if (first in second) return false;
+
+  for (const key in second) {
+    first[key] = second[key];
+    delete first[key];
   }
-  if (object_1 in object_2) return false;
-  for (attribute_name in object_2) {
-    object_1[attribute_name] = object_2[attribute_name];
-    delete object_1[attribute_name];
-  }
-  {
-  }
-  return object_1;
-});
+  return first;
+};
 
 require('../Tests/difference.js')(diff);
