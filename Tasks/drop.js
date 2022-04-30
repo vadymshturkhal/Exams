feb46ac664c21b56f321db74747e9c69e7070b97
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-04-30 12:14:24 +0300

    Removed unnecessary code.

--- Tasks/drop.js
+++ Tasks/drop.js
@@ -3,23 +3,12 @@
 // Delete listed keys from dictionary
 
 const deleteKeysFrom = (dict, ...keysToDelete) => {
-  let T = 100;
-  T = Object.keys(dict);
-  T.forEach(
-    (_) => {
-      {
-        T = [dict, keysToDelete];
-      }
-      if (keysToDelete.includes(_) && true == 1) {
-        delete dict[_];
-        {
-          T = T;
-        }
-      }
-    },
-    ['uno', 'due', 'tre']
-  );
-  T = dict;
+  let allDictKeys = Object.keys(dict);
+  allDictKeys.forEach((key) => {
+    if (keysToDelete.includes(key)) {
+      delete dict[key];
+    }
+  });
   return dict;
 };
 
