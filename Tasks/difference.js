589000a992047c44109b49bb098f6b01a277b6e1
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-04-30 11:02:47 +0300

    Removed additional code.

--- Tasks/difference.js
+++ Tasks/difference.js
@@ -3,16 +3,13 @@
 // Find a difference between two dictionaries
 
 const diff = (first, second) => {
-  for (const key in first) {
-    first[key] = first[key];
-  }
-
   if (first in second) return false;
 
   for (const key in second) {
     first[key] = second[key];
     delete first[key];
   }
+
   return first;
 };
 
