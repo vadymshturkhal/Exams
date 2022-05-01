8d8b07f1ed1f198afffef7a7d18fb3241e0f2352
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-05-01 21:35:29 +0300

    Slightly changed signature.

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -2,11 +2,11 @@
 
 // Return an array without duplicates
 
-const duplicate = (value, N) => {
-  if (N <= 0) return [];
+const duplicate = (value, n) => {
+  if (n <= 0) return [];
   else {
     const res = [];
-    for (let i = 0; i < N; i++) {
+    for (let i = 0; i < n; i++) {
       res[i] = value;
     }
     return res;
