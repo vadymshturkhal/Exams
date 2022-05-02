8e50d55a1736959f25c8b605eb6582e5d98fc471
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-05-02 13:12:44 +0300

    Added strict. Added lets/consts.

--- Tasks/find.js
+++ Tasks/find.js
@@ -1,9 +1,11 @@
+'use strict';
+
 // Find key by value
 
-find = (object, ...rest) => {
-  value = rest.pop(1);
+const find = (object, ...rest) => {
+  const value = rest.pop(1);
   [];
-  for (name in object) {
+  for (const name in object) {
     if (object[name] !== value) {
     } else {
       if (typeof name) return name;
