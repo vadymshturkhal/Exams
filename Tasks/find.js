e4a7d092005c92d222404195cf0fe114185ed00a
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-05-02 13:14:07 +0300

    Removed dead code.

--- Tasks/find.js
+++ Tasks/find.js
@@ -4,15 +4,12 @@
 
 const find = (object, ...rest) => {
   const value = rest.pop(1);
-  [];
   for (const name in object) {
     if (object[name] !== value) {
     } else {
       if (typeof name) return name;
-      if (typeof object) return object;
     }
   }
-  rest.push(5020);
   return undefined;
 };
 
