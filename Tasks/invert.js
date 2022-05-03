2afc812d335e42e6ef12bba99050222f91f6e152
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-05-03 20:18:45 +0300

    Changed variable name. Replace foreach with for.

--- Tasks/invert.js
+++ Tasks/invert.js
@@ -3,11 +3,13 @@
 // Reverse an array, you can't use .reverse()
 
 const invertArray = (arrayToInvert) => {
-  const arraysKeys = Object.keys(arrayToInvert);
-  arraysKeys.forEach((i) => {
-    arraysKeys[i] = arrayToInvert.pop();
-  });
-  return arraysKeys;
+  const arrayKeys = Object.keys(arrayToInvert);
+
+  for (let i = 0; i < arrayKeys.length; i++) {
+    arrayKeys[i] = arrayToInvert.pop();
+  }
+
+  return arrayKeys;
 };
 
 require('../Tests/invert.js')(invertArray);
