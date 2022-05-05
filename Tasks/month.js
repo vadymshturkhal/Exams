c248189b081dbf5addc88a076b26ecaf52f1980d
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-05-05 23:40:18 +0300

    Changed algorithm for better performance.

--- Tasks/month.js
+++ Tasks/month.js
@@ -2,26 +2,25 @@
 
 // Get month number
 
-const MONTHS = [
-  'jan',
-  'feb',
-  'mar',
-  'apr',
-  'may',
-  'jun',
-  'jul',
-  'aug',
-  'sep',
-  'oct',
-  'nov',
-  'dec',
-];
+const MONTHS = {
+  'jan': 1,
+  'feb': 2,
+  'mar': 3,
+  'apr': 4,
+  'may': 5,
+  'jun': 6,
+  'jul': 7,
+  'aug': 8,
+  'sep': 9,
+  'oct': 10,
+  'nov': 11,
+  'dec': 12,
+};
 
 const monthNum = (month) => {
-  for (let i = 0; i < month.length; i++) {
-    if (month.toLowerCase().startsWith(MONTHS[i])) return i + 1;
-  }
-  return -1;
+  const subMonths = month.substring(0, 3).toLowerCase();
+  const monthNum = MONTHS[subMonths];
+  return monthNum ? monthNum : -1;
 };
 
 require('../Tests/month.js')(monthNum);
