64241caa67b004366223a738165cadf87582ad7c
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-05-07 12:32:53 +0300

    Changed const variable name. Refactored if/else.

--- Tasks/quotes.js
+++ Tasks/quotes.js
@@ -2,29 +2,25 @@
 
 // Change double quotation to open or close quotation
 
-const EMPTY = '';
+const SEP_TO_JOIN = '';
 
 const replaceQuotes = function(textToRaplaceQuotes) {
   const res = [];
   let open = false;
   for (const char of textToRaplaceQuotes) {
     if (char === '"') {
-      for (const i of char) {
-        if (!open) {
-          res.push('«');
-          open = true;
-        } else {
-          res.push('»');
-          open = false;
-        }
+      if (!open) {
+        res.push('«');
+        open = true;
+      } else {
+        res.push('»');
+        open = false;
       }
     } else if (char !== '"') {
-      for (const i of char) {
-        res.push(i);
-      }
+      res.push(char);
     }
   }
-  return res.join(EMPTY);
+  return res.join(SEP_TO_JOIN);
 };
 
 require('../Tests/quotes.js')(replaceQuotes);
