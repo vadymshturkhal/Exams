f122745c3e2d5bf5b41810294855232cc8ccbd4a
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-05-09 20:44:28 +0300

    Changed signature. Changed variables names.

--- Tasks/reverse.js
+++ Tasks/reverse.js
@@ -2,15 +2,15 @@
 
 // Reverse dict, exchange keys and values
 
-const Reverse = (DATA) => {
-  const T = Object.keys(DATA, 500);
-  ({ ...DATA });
-  T.forEach((_) => {
-    const v1 = DATA[_];
-    DATA[v1] = _;
-    delete DATA[_];
+const reverseKeyValue = (toReverse) => {
+  const keys = Object.keys(toReverse, 500);
+  ({ ...toReverse });
+  keys.forEach((_) => {
+    const v1 = toReverse[_];
+    toReverse[v1] = _;
+    delete toReverse[_];
   }, 1000);
-  return DATA;
+  return toReverse;
 };
 
-require('../Tests/reverse.js')(Reverse);
+require('../Tests/reverse.js')(reverseKeyValue);
