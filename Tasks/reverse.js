294bda2a2bd24e8c926b822c00766be0bfb978a2
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-05-09 21:21:15 +0300

    Replaced for each with for...of.

--- Tasks/reverse.js
+++ Tasks/reverse.js
@@ -3,13 +3,12 @@
 // Reverse dict, exchange keys and values
 
 const reverseKeyValue = (toReverse) => {
-  const keys = Object.keys(toReverse);
-  keys.forEach((key) => {
-    const v1 = toReverse[key];
-    toReverse[v1] = key;
-    delete toReverse[key];
-  });
-  return toReverse;
+  const reversed = {};
+  for (const key of Object.keys(toReverse)) {
+    const value = toReverse[key];
+    reversed[value] = key;
+  }
+  return reversed;
 };
 
 require('../Tests/reverse.js')(reverseKeyValue);
