6b0e2e8af3715f62e6cb0684d8d8cfe29bda9364
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-05-10 23:45:12 +0300

    Removed else. Replaced compare operator.

--- Tasks/section.js
+++ Tasks/section.js
@@ -2,13 +2,12 @@
 
 // Split string into array by the first occurrence of separator
 
-const splitBySep = (s, t) => {
-  const i = s.indexOf(t);
-  if (i < 0 || t == '') {
-    return [s, ''];
-  } else {
-    return [s.slice(0, i), s.slice(i + t.length)];
+const splitBySep = (str, sep) => {
+  const i = str.indexOf(sep);
+  if (i < 0 || sep === '') {
+    return [str, ''];
   }
+  return [str.slice(0, i), str.slice(i + sep.length)];
 };
 
 require('../Tests/section.js')(splitBySep);
