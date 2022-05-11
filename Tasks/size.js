bf5f37c23e00463760dd1394eee1287fa9a99fae
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-05-11 17:26:33 +0300

    Added exps constant. Created additional variables.

--- Tasks/size.js
+++ Tasks/size.js
@@ -3,14 +3,21 @@
 // Convert number to file size in bytes, kb, mb, and gb
 
 const BASE = 1000;
+const EXP_NOTATIONS = {
+  0: ' byte',
+  1: ' kb',
+  2: ' mb',
+  3: ' gb',
+};
 
 const sizeToBytes = (size) => {
   if (size === 0) return '0 byte';
+
   const exp = Math.floor(Math.log(size) / Math.log(BASE));
-  if (exp === 0) return size + ' byte';
-  if (exp === 1) return Math.round(size / Math.pow(BASE, exp)) + ' kb';
-  if (exp === 2) return Math.round(size / Math.pow(BASE, exp)) + ' mb';
-  if (exp === 3) return Math.round(size / Math.pow(BASE, exp)) + ' gb';
+  if (exp === 0) return size + EXP_NOTATIONS[exp];
+
+  const formattedSize = Math.round(size / Math.pow(BASE, exp));
+  return formattedSize + EXP_NOTATIONS[exp];
 };
 
 require('../Tests/size.js')(sizeToBytes);
