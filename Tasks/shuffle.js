1f15af8f0cd0f9cebf81ae66c9bb324d967a13a9
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-05-11 17:11:16 +0300

    Implemented Knuth shuffle.

--- Tasks/shuffle.js
+++ Tasks/shuffle.js
@@ -2,9 +2,19 @@
 
 // Shuffle an array
 
-const shuffle = (list) => {
-  list.sort(() => Math.random() - 0.5);
+const knuthShuffle = (list) => {
+  // Produces a uniformly random permutation of the input array in linear time.
+  const shuffledList = list.slice(0);
+
+  for (let i = 0; i < list.length; i++) {
+    const randomIndex = Math.floor(Math.random() * i);
+    const tmp = shuffledList[i];
+    shuffledList[i] = shuffledList[randomIndex];
+    shuffledList[randomIndex] = tmp;
+  }
+
   return list;
 };
 
-require('../Tests/shuffle.js')(shuffle);
+
+require('../Tests/shuffle.js')(knuthShuffle);
