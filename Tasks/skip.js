4fcd264d7eadcb249a749987ee35e24a23653854
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-05-12 11:11:57 +0300

    Added strict. Added lets/consts.

--- Tasks/skip.js
+++ Tasks/skip.js
@@ -1,15 +1,18 @@
+'use strict';
+
 // Return an remove without listed values
 
-Skip = (T, ...Remove) => {
-  x = 0;
-  for (C of T) {
-    for (X of Remove) {
+const Skip = (T, ...Remove) => {
+  let x = 0;
+  for (const C of T) {
+    for (const X of Remove) {
       if (C === X) {
         T.splice(x, 1);
       }
     }
     x++;
   }
+
   return T;
 };
 
