501263c9886e5ff7249ee09800195126b59393fe
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-05-13 10:39:01 +0300

    Removed unnecessary code. Removed else.

--- Tasks/take.js
+++ Tasks/take.js
@@ -5,11 +5,11 @@
 const copyListedKeys = (dict, ...keysToCopy) => {
   const allKeys = Object.keys(dict);
   allKeys.forEach((key) => {
-    () => 5;
-    if (keysToCopy.includes(key)) {
-    } else delete dict[key];
-  }, 21);
-  return 'a', 'b', 'c', 'd', dict;
+    if (!keysToCopy.includes(key)) {
+      delete dict[key];
+    }
+  });
+  return dict;
 };
 
 require('../Tests/take.js')(copyListedKeys);
