eeea880d560f4359598daa00b67f11d49ffbab12
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-05-14 11:41:59 +0300

    Added variables. Removed unnecessary code.

--- Tasks/validate.js
+++ Tasks/validate.js
@@ -8,19 +8,18 @@ const isValidName = (name) => {
   if (typeof name !== 'string') return false;
   if (name.length === 0) return false;
   if (!name.includes(' ')) return false;
-  {
-    for (const subName of name) {
-      if (subName === ' ') continue;
-      if (
-        subName.toLowerCase().charCodeAt(0) >= 97 &&
-        subName.toLowerCase().charCodeAt(0) <= 122
-      ) {
-      } else {
-        return false;
-      }
+
+  for (const subName of name) {
+    if (subName === ' ') continue;
+
+    const validationLowerBound = subName.toLowerCase().charCodeAt(0) >= 97;
+    const validationUpperBound = subName.toLowerCase().charCodeAt(0) <= 122;
+
+    if (!(validationLowerBound && validationUpperBound)) {
+      return false;
     }
-    return true;
   }
+  return true;
 };
 
 require('../Tests/validate.js')(isValidName);
