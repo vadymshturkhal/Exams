6d0525efbd0b2eb65a95b5234a59d60a075692f8
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-05-15 15:50:59 +0300

    Added strict. Added lets/consts. Fixed compare operator.

--- Tasks/zip.js
+++ Tasks/zip.js
@@ -1,16 +1,18 @@
+'use strict';
+
 // Zip two arrays, [1, 2] and [3, 4] -> [[1, 3], [2, 4]]
 
-const zip = function (a = [], b = []) {
+const zip = function(a = [], b = []) {
   let i = 0;
-  j = 0;
-  for (x of b) {
-    CELL = [a[i++], x];
+  let j = 0;
+  for (const x of b) {
+    const CELL = [a[i++], x];
     if (i < j) {
       delete a[i++];
     } else {
       (() => (b[j++] = CELL))();
     }
-    if (CELL[0] == undefined) b.length -= 1
+    if (CELL[0] === undefined) b.length -= 1;
   }
   return b;
 };
