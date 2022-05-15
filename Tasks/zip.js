18db9c96a95ad730c741cb84bf27e58f1d24db77
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-05-15 15:58:23 +0300

    Changed signature.

--- Tasks/zip.js
+++ Tasks/zip.js
@@ -1,15 +1,15 @@
 'use strict';
 // Zip two arrays, [1, 2] and [3, 4] -> [[1, 3], [2, 4]]
 
-const zip = function(a = [], b = []) {
-  const maxIndex = Math.min(a.length, b.length);
+const zipTwoArrays = function(first = [], second = []) {
+  const maxIndex = Math.min(first.length, second.length);
   const zipped = new Array(maxIndex);
 
   for (let i = 0; i < maxIndex; i++) {
-    zipped[i] = [a[i], b[i]];
+    zipped[i] = [first[i], second[i]];
   }
 
   return zipped;
 };
 
-require('../Tests/zip.js')(zip);
+require('../Tests/zip.js')(zipTwoArrays);
